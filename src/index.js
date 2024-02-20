c71340734679ce8485c3fcb4edc7e9e7559351e2
gokuthecoder <vishalkr12012001@gmail.com>
2024-02-20 09:40:53 +0530

    .env file created

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
+import dotenv from 'dotenv'
 import connectDB from "./db/index.js";
-import dotenv from "dotenv"
 import { app } from "./app.js";
 
-dotenv.config({
-    path: './env'
-})
 
 connectDB()
     .then(() => {
